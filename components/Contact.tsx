2597a816df27409395a5981e0ecdb18105ecadfb
susanthkurma <susanthkurma3@gmail.com>
2026-07-15 16:19:11 +0530

    official icons added

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin, Mail } from "lucide-react";
+import Image from "next/image";
 
 export default function Contact() {
   return (
@@ -6,40 +6,59 @@ export default function Contact() {
       id="contact"
       className="py-20 bg-white text-gray-900"
     >
-      <div className="max-w-5xl mx-auto px-6">
+      <div className="max-w-5xl mx-auto px-6 text-center">
 
-        <h2 className="text-4xl font-bold text-center mb-12">
+        <h2 className="text-4xl font-bold mb-10">
           Let's Connect
         </h2>
 
-        <div className="flex justify-center gap-10">
+        <div className="flex justify-center gap-8">
 
           <a
-            href="https://github.com/susanthkurma/portfolio"
+            href="https://github.com/susanthkurma"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:scale-110 transition duration-300"
-            aria-label="GitHub"
           >
-            <Github size={45} />
+            <Image
+              src="/icons/github.png"
+              alt="GitHub"
+              width={50}
+              height={50}
+              className="hover:scale-110 transition duration-300"
+            />
           </a>
 
           <a
             href="https://linkedin.com/in/susanth-kurma-56aa76160"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:scale-110 transition duration-300 text-blue-600"
-            aria-label="LinkedIn"
           >
-            <Linkedin size={45} />
+            <Image
+              src="/icons/linkedin.png"
+              alt="LinkedIn"
+              width={50}
+              height={50}
+              className="hover:scale-110 transition duration-300"
+            />
           </a>
 
-          <a
-            href="mailto:susanthkurma3@gmail.com"
-            className="hover:scale-110 transition duration-300 text-red-500"
-            aria-label="Email"
-          >
-            <Mail size={45} />
+          <a href="mailto:susanthkurma3@gmail.com">
+            <Image
+              src="/icons/gmail.png"
+              alt="Email"
+              width={50}
+              height={50}
+              className="hover:scale-110 transition duration-300"
+            />
+          </a>
+          <a href="">
+            <Image
+              src="/icons/instagram.png"
+              alt="Instagram"
+              width={50}
+              height={50}
+              className="hover:scale-110 transition duration-300"
+            />
           </a>
 
         </div>
