19461d78dec01678eecc224e5357563d4048c2af
susanthkurma <susanthkurma3@gmail.com>
2026-07-15 19:58:59 +0530

    download cv added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -80,6 +80,56 @@ export default function Hero() {
 
       </div>
 
+      <div className="mt-10 flex flex-wrap gap-4">
+
+  <a
+    href="/resume.pdf"
+    download
+    className="
+      inline-flex
+      items-center
+      justify-center
+      px-8
+      py-4
+      bg-cyan-500
+      text-white
+      font-semibold
+      rounded-xl
+      hover:bg-cyan-400
+      transition-all
+      duration-300
+      shadow-lg
+      hover:shadow-cyan-500/40
+      hover:-translate-y-1
+    "
+  >
+    Download CV
+  </a>
+
+  <a
+    href="#projects"
+    className="
+      inline-flex
+      items-center
+      justify-center
+      px-8
+      py-4
+      border
+      border-cyan-500
+      text-cyan-400
+      font-semibold
+      rounded-xl
+      hover:bg-cyan-500
+      hover:text-white
+      transition-all
+      duration-300
+    "
+  >
+    View Projects
+  </a>
+
+</div>
+
     </section>
   );
 }
