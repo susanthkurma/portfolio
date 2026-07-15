332e1ada2e4fa85e53089e0e741cfc6d9be94a60
susanthkurma <susanthkurma3@gmail.com>
2026-07-15 17:57:44 +0530

    name removed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,10 +5,7 @@ export default function Navbar() {
     <nav className="fixed top-0 w-full bg-slate-950/90 backdrop-blur-md border-b border-slate-800 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-8 py-5">
 
-        <h1 className="text-3xl font-bold text-cyan-400">
-          Susanth Kurma
-        </h1>
-
+        
         <div className="flex gap-10 text-white font-medium">
 
           <Link href="#about" className="hover:text-cyan-400 transition">
